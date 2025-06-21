7790efa14c6681cb9bab6e3ba9b436e032ae8e98
Abubakr123 <your.email@example.com>
2025-06-21 04:14:38 +0200

    Build site to /docs for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'static',
+  base: '/',
   build: {
     outDir: 'docs',
   },
-  base: '/',
   integrations: [tailwind()],
 });
