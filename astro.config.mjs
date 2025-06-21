286663af27c8fd046be76633e443a2b40beed08c
Abubakr123 <your.email@example.com>
2025-06-21 02:53:18 +0200

    Add GitHub Pages adapter and build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  output: 'static',
+  base: '/',
   integrations: [tailwind()],
   server: {
     host: '0.0.0.0',
-    port: 3000
-  }
+    port: 3000,
+  },
 });
